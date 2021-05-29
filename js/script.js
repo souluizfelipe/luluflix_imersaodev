c2d0ff147dfe230e0ddb7d75f3b65c35930acd66
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-05-29 10:13:40 -0300

    finish displaymovie obj that return movie banner and movie name

--- js/script.js
+++ js/script.js
@@ -1,30 +1,62 @@
 const moviesList = [];
 
+const displayMovie = {
+    movieNameRaw: document.querySelector('.main-input.-name'),
+    movieBannerRaw: document.querySelector('.main-input.-banner'),
 
-const displaymovie = {
-
-    inputContent() {
-        
+    inputContentName() {
+        console.log(this.movieNameRaw.value);
+        return displayMovie.movieNameRaw.value;
     },
 
-    displayAddContent() {
+    inputContentBanner() {
+        console.log(this.movieBannerRaw.value);
+        return displayMovie.movieBannerRaw.value;
+        // return utils.verifyURL(displayMovie.movieBannerRaw.value);
+    },
+   
+    addMovie() {
         const listBanners = document.querySelector('.list-banners');
         const figure = document.createElement('figure');
         const img = document.createElement('img');
         const figcaption = document.createElement('figcaption');
         
         figure.classList.add('wrapper-banner');
-        img.src = 'img/banner2.jpg';
+        img.src = `${displayMovie.inputContentBanner()}`;
         img.alt = 'movie banner';
+        
         img.classList.add('.main-banner');
-        figcaption.textContent = 'Nome do filme';
+        figcaption.textContent = `${displayMovie.inputContentName()}`;
         figcaption.classList.add('main-caption');        
         
         figure.insertAdjacentElement('beforeend', img);
         figure.insertAdjacentElement('beforeend', figcaption);
         listBanners.insertAdjacentElement('beforeend', figure);
+        
+        moviesList.push()
+    },
+    
+    submitContent() {
+        displayMovie.addMovie();
+        utils.clearInput(); 
     },
 };
 
+const utils = {
+    // verifyURL() { 
+    //      (inp) => {
+    //         inp = inp.value;
+    //         if (inp.endsWith('.jpg')) {
+    //             return inp.value;
+    //         } else { 
+    //             return alert('Insira uma URL valida');
+    //         } 
+    //     }
+    // },
 
-displaymovie.displayAddContent();
+    clearInput() {
+        displayMovie.movieNameRaw.value = '';
+        displayMovie.movieBannerRaw.value = '';
+    },
+}
+
